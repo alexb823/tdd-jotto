a0792f69570c6e2edfb47e40bf2c1c2553ddc812
Alex Bukhman <alexb823@gmail.com>
2020-03-15 11:08:07 -0400

    added redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,6 +1,7 @@
 import checkPropTypes from 'check-prop-types';
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/store';
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
@@ -16,6 +17,10 @@ export const checkProps = (component, conformingProps) => {
   expect(propError).toBeUndefined();
 };
 
-export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
-}
+export const storeFactory = initialState => {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
+};
