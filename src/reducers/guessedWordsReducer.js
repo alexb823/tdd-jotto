c2afeab95ed787a30cfa4c33bbd278629bcc0f77
Alex Bukhman <alexb823@gmail.com>
2020-03-29 17:01:46 -0400

    added guessWord action creater

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,11 +1,10 @@
-import { actionTypes } from '../actions';
+import {actionTypes} from '../actions';
 
 export default (state = [], action) => {
-  // switch (action.type) {
-  //   case actionTypes.GUESS_WORD:
-  //     return [...state, action.payload];
-  //   default:
-  //     return state;
-  // }
-  return null;
-};
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+      default:
+        return state;
+  }
+}
